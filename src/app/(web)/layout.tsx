dac240fa02abb80507c76c7257b0e23d90bb858f
Ako Kyaw <macbookpro@Davids-MacBook-Pro.local>
2024-03-03 17:01:46 +0630

    final

---
+++ src/app/(web)/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+import { Barlow } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import ThemeProvider from "@/components/ThemeProvider/ThemeProvider";
+import { NextAuthProvider } from "@/components/AuthProvider/AuthProvider";
+import Toast from "@/components/Toast/Toast";
+
+const barlow = Barlow({ 
+  subsets: ["latin"],
+  weight : ["400","500","700","900"],
+  style : ["italic","normal"],
+  variable : "--font-barlow",
+  adjustFontFallback : false,
+});
+
+export const metadata: Metadata = {
+  title: "Babylon",
+  description: "Discover the best hotel rooms",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+          crossOrigin='anonymous'
+        />
+      </head>
+      <body className={barlow.className}>
+        <NextAuthProvider>
+          <ThemeProvider>
+            <Toast/>
+            <main className="font-normal">
+              <Header/>
+              {children}
+              <Footer/>
+            </main>
+          </ThemeProvider>
+        </NextAuthProvider>
+      </body>
+    </html>
+  );
+}
